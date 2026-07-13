b9ce9842729a34b5e041ce7ba4b51956276ba4d4
colinknox <colinwknox@gmail.com>
2026-07-12 20:13:50 -0400

    Literally forgot the print statem 🤦‍♂️

--- madlibs.py
+++ madlibs.py
@@ -8,4 +8,6 @@ verb_one = input("Verb: ")
 verb_two = input("Verb: ")
 verb_three = input("Verb: ")
 
-madlib = f"I live on a {noun_one}.\nIn this {noun_one}, the {adjective_three}, {noun_two} {verb_two} from the sky.\nWhen the local {noun_three}, came {adjective_two} to the scene,\nthey {verb_one} and {verb_three} at the mere sight of the {verb_three}"
+madlib = f"I live on a {noun_one}.\nIn this {noun_one}, the {adjective_three}, {noun_two} {verb_two} from the sky.\nWhen the local {noun_three}, came {adjective_two} to the scene,\nthey {verb_one} and {verb_three} at the mere sight of the {verb_three}."
+
+print(madlib)
